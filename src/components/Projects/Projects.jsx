d6a258677ec1e49abea1526543838e81b3d0a95a
aurelia Segarra <av.segarra@gmail.com>
2021-02-19 10:43:55 +0100

    btn projects ok

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -52,7 +52,7 @@ const Projects = () => {
                       <a
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
+                        className="cta-btn cta-btn--hero--eye"
                         href={url || '#!'}
                       >
                         See Live
